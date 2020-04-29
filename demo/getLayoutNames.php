6ce21bb4b12639390f677695749b1c9f10956bfc
Filip Skřivánek <filip.skrivanek@24usoftware.com>
2020-04-29 10:26:05 +0200

    Init commit

---
+++ demo/getLayoutNames.php
@@ -0,0 +1,12 @@
+<?php
+
+use fmRESTor\fmRESTor;
+session_start();
+require_once dirname(__DIR__) . '/src/fmRESTor.php';
+
+$fm = new fmRESTor("127.0.0.1", "fmRESTor", "php_user", "api", "api123456", array("allowInsecure" => true));
+
+// Get all avaiable layouts names for given database
+$response = $fm->getLayoutNames();
+var_dump($response);
+exit();
